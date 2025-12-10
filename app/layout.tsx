0ba45058d437285202854d48b59ecc16ad3af74b
mdjameel04 <jameeltony67@gmail.com>
2025-12-10 17:35:57 +0530

    inital commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import {Outfit } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
  import { ClerkProvider} from '@clerk/nextjs'
+import { ConvexClientProvider } from "./ConvexClientProvider";
 const outfit=Outfit({subsets:["latin"],
   variable: "--font-outfit",})
 
@@ -22,9 +23,9 @@ export default function RootLayout({
       <body
         className={`${outfit.className}`}
       >
-        <Provider>
+        <ConvexClientProvider>
         {children}
-        </Provider>
+       </ConvexClientProvider>
         
       </body>
     </html>
